7421a006db767daa3ffb633e116bda12b5814526
Methewdev <envidevelopment@gmail.com>
2026-06-04 21:32:14 +0700

    Update appy_3.py

--- appy_3.py
+++ appy_3.py
@@ -1,4 +1,54 @@
 import streamlit as st
+import pandas as pd
+import torch
 
-st.title("TEST APP")
-st.success("Deploy berhasil")
+from transformers import (
+    AutoTokenizer,
+    AutoModelForSequenceClassification
+)
+
+# =====================
+# PAGE CONFIG
+# =====================
+
+st.set_page_config(
+    page_title="Emotion AI",
+    layout="wide"
+)
+
+# =====================
+# LOAD MODEL
+# =====================
+
+@st.cache_resource
+def load_models():
+
+    sentiment_tokenizer = AutoTokenizer.from_pretrained(
+        "envidevelopment/sentiment_model"
+    )
+
+    sentiment_model = AutoModelForSequenceClassification.from_pretrained(
+        "envidevelopment/sentiment_model"
+    )
+
+    emotion_tokenizer = AutoTokenizer.from_pretrained(
+        "envidevelopment/emotion_model"
+    )
+
+    emotion_model = AutoModelForSequenceClassification.from_pretrained(
+        "envidevelopment/emotion_model"
+    )
+
+    return (
+        sentiment_tokenizer,
+        sentiment_model,
+        emotion_tokenizer,
+        emotion_model
+    )
+
+(
+    sentiment_tokenizer,
+    sentiment_model,
+    emotion_tokenizer,
+    emotion_model
+) = load_models()
