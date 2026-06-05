471cff7d3a8718a990b0762b02c52034be161871
Methewdev <envidevelopment@gmail.com>
2026-06-05 21:44:41 +0700

    Update appy_3.py

--- appy_3.py
+++ appy_3.py
@@ -302,7 +302,10 @@ elif menu == "Bulk CSV":
 
         try:
 
-            df = pd.read_csv(uploaded_file)
+           df = pd.read_csv(
+    uploaded_file,
+    encoding="cp1252"
+)
 
             st.dataframe(
                 df.head()
